68bccc5b37377ef9816a25ae67e73b7120818f26
pascallosss <53646294+pascallosss@users.noreply.github.com>
2019-08-03 13:00:46 +0200

    remove todo

--- test.js
+++ test.js
@@ -1,15 +1,6 @@
 var kaas = "kaas"
 console.log('dssdssdsdds');
 
-// todo: lalalaa
-// hack: test code climate
-
-
-// todo: a lot
-
-
-// TODO neeee
-
 var kaas2 = "kaaassss";
 
 console.log('help');
